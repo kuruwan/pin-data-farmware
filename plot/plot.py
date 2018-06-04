0c9ddeac906bb769be959ffdc6d2bd6eb84f0c86
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-06-04 16:35:26 -0700

    allow sensor pin number input

--- plot/plot.py
+++ plot/plot.py
@@ -10,38 +10,37 @@ import requests
 import numpy as np
 import cv2
 
-PIN = 59
-LOCAL_STORE = 'pin_data_' + str(PIN)
+FARMWARE_NAME = 'plot_sensor_data'
 TIME_SCALE_FACTOR = 60 * 2
 DATA_SCALE_FACTOR = 2
 RECENT = {'time': None}
 
-def api_url():
-    major_version = int(os.getenv('FARMBOT_OS_VERSION', '0.0.0')[0])
-    base_url = os.environ['FARMWARE_URL']
-    return base_url + 'api/v1/' if major_version > 5 else base_url
+def get_env(key, type_=int):
+    'Return the value of the namespaced Farmware input variable.'
+    return type_(os.getenv('{}_{}'.format(FARMWARE_NAME, key), 59))
 
 def post(wrapped_data):
     """Send the Celery Script command."""
     headers = {
         'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-        'content-type': "application/json"}
+        'content-type': 'application/json'}
     payload = json.dumps(wrapped_data)
-    requests.post(api_url() + 'celery_script',
+    requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
                   data=payload, headers=headers)
 
 def no_data_error():
     """Send an error to the log if there's no data."""
+    message = '[Plot sensor data] No data available for pin {}.'.format(PIN)
     wrapped_message = {
-        "kind": "send_message",
-        "args": {
-            "message_type": "error",
-            "message": "[Plot sensor data] No data available."}}
+        'kind': 'send_message',
+        'args': {
+            'message_type': 'error',
+            'message': message}}
     post(wrapped_message)
 
-def get_pin_data():
+def get_pin_data(pin):
     """Get existing historical pin data."""
-    data = json.loads(os.getenv(LOCAL_STORE, '[]'))
+    data = json.loads(os.getenv('pin_data_' + str(pin), '[]'))
     if len(data) < 1:
         no_data_error()
         sys.exit(0)
@@ -57,7 +56,7 @@ def reduce_data(data):
     RECENT['time'] = max(times) * TIME_SCALE_FACTOR
     times = abs(np.array(times) - max(times))
     all_data = np.column_stack((times, values))
-    filtered_data = all_data[all_data[:, 0] < 720 ]
+    filtered_data = all_data[all_data[:, 0] < 720]
     return filtered_data
 
 def plot(data):
@@ -65,13 +64,14 @@ def plot(data):
     # Create blank plot
     p = np.full([512, 24 * 60 / 2], 255, np.uint8)
     # Add shaded plot areas
-    for i in range(512):
-        if i < 100:  # N/A
-            p[i, :] = 220
-        elif i > 425:  # off
-            p[i, :] = 220
-        else:  # sensor range (gradient)
-            p[i, :] = 255 - 175 * ((i - 100) / float(425 - 100))
+    if IS_SOIL_SENSOR:
+        for i in range(512):
+            if i < 100:  # N/A
+                p[i, :] = 220
+            elif i > 425:  # off
+                p[i, :] = 220
+            else:  # sensor range (gradient)
+                p[i, :] = 255 - 175 * ((i - 100) / float(425 - 100))
     # Add horizontal gridlines
     for i in range(0, 512, 32):
         p[i, :] = 100
@@ -87,9 +87,9 @@ def plot(data):
     cv2.rectangle(p, (0, 0), (719, 511), 50, 4)
     # Add data
     for record in data:
-        time = int(record[0])
-        value = int(record[1])
-        cv2.circle(p, (time, value), 5, 0, 3)
+        reading_time = int(record[0])
+        reading_value = int(record[1])
+        cv2.circle(p, (reading_time, reading_value), 5, 0, 3)
     # Flip plot to display oldest to newest, low to high
     p = cv2.flip(p, -1)
     # Create plot border label area
@@ -103,7 +103,8 @@ def plot(data):
                     {'text': 'wet', 'position': (425, 25)},
                     {'text': 'dry', 'position': (160, 25)},
                     {'text': 'n/a', 'position': (75, 25)}]
-    _add_labels(border, range_labels)
+    if IS_SOIL_SENSOR:
+        _add_labels(border, range_labels)
     # Flip labels to display vertically
     full = cv2.flip(cv2.transpose(border), 0)
     # Add sensor value labels
@@ -121,7 +122,8 @@ def plot(data):
     # Add label area to plot area
     full[44:556, 40:760] = p
     # Add plot title
-    cv2.putText(full, 'soil sensor'.upper(), (325, 25), 0, 0.75, 0, 2)
+    title = '{}sensor (pin {})'.format('soil ' if IS_SOIL_SENSOR else '', PIN)
+    cv2.putText(full, title.upper(), (325, 25), 0, 0.75, 0, 2)
     return full
 
 def save(image):
@@ -129,5 +131,7 @@ def save(image):
     filename = '/sensor_data_plot_{}.png'.format(int(time()))
     cv2.imwrite(os.environ['IMAGES_DIR'] + filename, image)
 
-if __name__ == "__main__":
-    save(plot(reduce_data(get_pin_data())))
+if __name__ == '__main__':
+    PIN = get_env('pin')
+    IS_SOIL_SENSOR = PIN == 59
+    save(plot(reduce_data(get_pin_data(PIN))))
--- save/save.py
+++ save/save.py
@@ -7,41 +7,35 @@ import sys
 import json
 from time import time
 import requests
-try:
-  import redis
-  legacy = True
-except ImportError:
-  legacy = False
 
-headers = {
-  'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-  'content-type': "application/json"}
+FARMWARE_NAME = 'save_sensor_data'
+HEADERS = {
+    'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
+    'content-type': 'application/json'}
 
-PIN = 59
-LOCAL_STORE = 'pin_data_' + str(PIN)
+def get_env(key, type_=int):
+    """Return the value of the namespaced Farmware input variable."""
+    return type_(os.getenv('{}_{}'.format(FARMWARE_NAME, key), 59))
 
 def no_data_error():
     """Send an error to the log if there's no data."""
+    message = '[Save sensor data] Pin {} value not available.'.format(PIN)
     wrapped_message = {
-        "kind": "send_message",
-        "args": {
-            "message_type": "error",
-            "message": "[Save sensor data] Pin value not available."}}
+        'kind': 'send_message',
+        'args': {
+            'message_type': 'error',
+            'message': message}}
     post(wrapped_message)
 
 def get_pin_value(pin):
-    """Get the value read by a Sequence `Read Pin` step."""
-    if legacy:
-        r = redis.Redis()
-        value = r.get('BOT_STATUS.pins.{}.value'.format(pin))
-    else:
-        response = requests.get(
-            os.environ['FARMWARE_URL'] + 'api/v1/bot/state',
-            headers=headers)
-        try:
-            value = response.json()['pins'][str(PIN)]['value']
-        except KeyError:
-            value = None
+    """Get the value read by a Sequence `Read Pin` step or the Sensor widget."""
+    response = requests.get(
+        os.environ['FARMWARE_URL'] + 'api/v1/bot/state',
+        headers=HEADERS)
+    try:
+        value = response.json()['pins'][str(pin)]['value']
+    except KeyError:
+        value = None
     if value is None:
         no_data_error()
         sys.exit(0)
@@ -49,7 +43,7 @@ def get_pin_value(pin):
 
 def timestamp(value):
     """Add a timestamp to the pin value."""
-    return {"time": time(), "value": value}
+    return {'time': time(), 'value': value}
 
 def append(data):
     """Add new data to existing data."""
@@ -57,28 +51,28 @@ def append(data):
         existing_data = json.loads(os.environ[LOCAL_STORE])
     except KeyError:
         existing_data = []
-    else:
-        existing_data.append(data)
+    existing_data.append(data)
     return existing_data
 
 def wrap(data):
     """Wrap the data in a `set_user_env` Celery Script command to save it."""
     return {
-        "kind": "set_user_env",
-        "args": {},
-        "body": [{
-            "kind": "pair",
-            "args": {
-                "label": LOCAL_STORE,
-                "value": json.dumps(data)
+        'kind': 'set_user_env',
+        'args': {},
+        'body': [{
+            'kind': 'pair',
+            'args': {
+                'label': LOCAL_STORE,
+                'value': json.dumps(data)
             }}]}
 
 def post(wrapped_data):
     """Send the Celery Script command."""
     payload = json.dumps(wrapped_data)
-    api_path = '' if legacy else 'api/v1/'
-    requests.post(os.environ['FARMWARE_URL'] + api_path + 'celery_script',
-                  data=payload, headers=headers)
+    requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
+                  data=payload, headers=HEADERS)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    PIN = get_env('pin')
+    LOCAL_STORE = 'pin_data_' + str(PIN)
     post(wrap(append(timestamp(get_pin_value(PIN)))))
